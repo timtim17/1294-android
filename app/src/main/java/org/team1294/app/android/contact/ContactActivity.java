8da4787e9422de7da2a7669612414b088e58a92e
Austin Jenchi <ajenchi9+code@gmail.com>
2014-12-26 11:46:17 -0800

    Add contact activity

---
+++ app/src/main/java/org/team1294/app/android/contact/ContactActivity.java
@@ -0,0 +1,14 @@
+package org.team1294.app.android.contact;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import org.team1294.app.android.R;
+
+public class ContactActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
